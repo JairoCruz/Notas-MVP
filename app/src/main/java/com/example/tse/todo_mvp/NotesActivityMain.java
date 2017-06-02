38977daeb74bf9c5c38287a708c4accbc3d2186b
JairoCruz <azaveliz@gmail.com>
2017-06-02 14:13:02 -0600

    agregue el diseño a layout principal, configuracion de un Toolbar, DrawerLayout y un Floating Action button.

--- app/src/main/java/com/example/tse/todo_mvp/NotesActivityMain.java
+++ app/src/main/java/com/example/tse/todo_mvp/NotesActivityMain.java
@@ -1,13 +1,45 @@
 package com.example.tse.todo_mvp;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
 
 public class NotesActivityMain extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_main);
+
+
+        // Configuracion del toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // Configuracion del Navigation Drawer
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+            // Abrir el Navigation Drawer cuando el icono de Home es seleccionado de el toolbar
+            drawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
